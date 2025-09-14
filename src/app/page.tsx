a08a17a946fbb81882dfd88a75351d651df6eeb1
AndresT2002 <andres.toloza@udea.edu.co>
2025-09-14 01:52:06 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [color, setColor] = useState("blue");
+  const colors = new Map<number, string>();
+  colors.set(1, "bg-blue-500");
+  colors.set(-1, "bg-red-500");
+
+  const [color, setColor] = useState(1);
   return (
     <div className=" flex flex-col items-center justify-center h-screen">
       <h1>Hello World</h1>
       This is a new project to be deployed on Vercel
       <button
-        className={`bg-${color}-500 text-white p-2 rounded-md`}
-        onClick={() => setColor("red")}
+        className={`${colors.get(
+          color
+        )} text-white p-2 rounded-md border border-white`}
+        onClick={() => setColor(color * -1)}
       >
-        Click me to change the color to {color}
+        Click me to change the color to {colors.get(color * -1)}
       </button>
     </div>
   );
